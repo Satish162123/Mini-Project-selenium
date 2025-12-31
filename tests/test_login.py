b8a157566b1351ad521120fff6a7b01ef6e88ac5
Satish162123 <satishkadarinpgs@gmail.com>
2025-12-31 22:07:29 +0530

    Added test with test planning and different test case design

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,25 +1,64 @@
-import pytest
-from pages.login_page import LoginPage
-from pages.inventory_page import InventoryPage
-from utils.config_reader import ConfigReader
+import pytest 
+from pages.login_page import LoginPage 
+from pages.inventory_page import InventoryPage 
+from utils.config_reader import ConfigReader 
+ 
+ 
+@pytest.mark.smoke 
+def test_login_with_valid_credentials(driver): 
+    """ 
+    TC01: Verify user can login with valid credentials 
+    """ 
+    login = LoginPage(driver) 
+    inventory = InventoryPage(driver) 
+ 
+    login.load() 
+    login.login( 
+        ConfigReader.get("credentials", "valid_username"), 
+        ConfigReader.get("credentials", "valid_password") 
+    ) 
+ 
+    assert inventory.is_loaded() 
+ 
+ 
+@pytest.mark.regression 
+def test_login_with_invalid_credentials(driver): 
+    """ 
+    TC02: Verify error message for invalid login 
+    """ 
+    login = LoginPage(driver) 
+ 
+    login.load() 
+    login.login( 
+        ConfigReader.get("credentials", "invalid_username"), 
+        ConfigReader.get("credentials", "invalid_password") 
+    ) 
+ 
+    assert "Username and password do not match" in login.get_error_message()
 
-@pytest.mark.smoke
-def test_valid_login(driver):
-    login_page = LoginPage(driver)
-    inventory_page = InventoryPage(driver)
-    login_page.load()
-    login_page.login(ConfigReader.get("credentials", "valid_username"),
-                     ConfigReader.get("credentials", "valid_password"))
-    
-    assert inventory_page.is_loaded()
-    #assert False
 
-@pytest.mark.regression
-def test_invalid_login(driver):
-    login_page = LoginPage(driver)
-    login_page.load()
-    login_page.login(ConfigReader.get("credentials", "invalid_username"),
-                     ConfigReader.get("credentials", "invalid_password"))
-    
-    assert "Username and password do not match" in login_page.get_error_message()
-    #assert False
+ 
+@pytest.mark.regression 
+def test_login_with_empty_username(driver): 
+    """ 
+    TC03: Verify error when username is empty 
+    """ 
+    login = LoginPage(driver) 
+ 
+    login.load() 
+    login.login("", "some_password") 
+ 
+    assert "Username is required" in login.get_error_message() 
+ 
+ 
+@pytest.mark.regression 
+def test_login_with_empty_password(driver): 
+    """ 
+    TC04: Verify error when password is empty 
+    """ 
+    login = LoginPage(driver) 
+ 
+    login.load() 
+    login.login("standard_user", "") 
+ 
+    assert "Password is required" in login.get_error_message()
